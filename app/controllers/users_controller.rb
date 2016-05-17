467f740c73b900b79cea965e37fff18cb266096a
Skim0082 <joe215.kim@gmail.com>
2016-05-17 05:43:59 +0000

    Improve to show only active users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,9 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
   
   def index
-    @users = User.paginate(page: params[:page], per_page: '5')
+    #@users = User.paginate(page: params[:page], per_page: '5')
+    # Shows only active users
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: '5')
   end
   
   def new
@@ -13,6 +15,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    redirect_to root_url unless @user.activated?
   end
   
   def create
